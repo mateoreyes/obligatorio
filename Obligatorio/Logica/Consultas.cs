c3e7a68cd015f1eb349024dfca0fba5ba3d11622
mateoreyes <rchmateo@gmail.com>
2018-11-02 20:18:59 -0300

    Add files via upload

---
+++ Obligatorio/Logica/Consultas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+    public class Consultas
+    {
+        public String profesionalesObtener()
+        {
+            String consulta = "select * from profesional";
+
+            return consulta;
+        }
+    }
+}
